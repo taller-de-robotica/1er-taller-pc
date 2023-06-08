ee7f764d0d731d8f6c101a34d311600021343565
Vero <v.arriola@ciencias.unam.mx>
2023-06-08 14:42:02 -0600

    Refactorización del código para ejecutar sobre imágenes o video dependiendo de los argumentos de entrada.  Separación de predición y presentación del resultado como imágenes.

--- paneles/poly_dust_detector.py
+++ paneles/poly_dust_detector.py
@@ -21,22 +21,24 @@ Modifications for robotic system:
 """
 
 #LIBRARIES
-from keras.models import load_model
 import numpy as np
 from matplotlib import pyplot as plt
 from patchify import patchify, unpatchify
 import cv2
-import segmentation_models as sm
 import matplotlib as mpl
 mpl.rcParams['figure.dpi'] = 330
 
+import argparse
 from termcolor import cprint
 MC = 'yellow'
+
+image_size = (1792, 1024)
+
 
 #LOAD_IMAGE FUNCTION
 def load_image(path, import_type='bgr_img'):
     img=cv2.imread(path)
-    res_img = cv2.resize(img, (1792, 1024), interpolation = cv2.INTER_LINEAR)
+    res_img = cv2.resize(img, image_size, interpolation = cv2.INTER_LINEAR)
     if import_type == 'gray_img':
         load_img=cv2.cvtColor(res_img,cv2.COLOR_BGR2GRAY)
     elif import_type == 'bgr_img':
@@ -49,32 +51,20 @@ def load_image(path, import_type='bgr_img'):
     return load_img
 
 #UNET_PREDICTION FUNCTION
-def unet_prediction(ip_address=None, image=None, model=None):
-    cprint("Reading image...", MC)
+def unet_prediction(image, model=None):
+    """
+    Receives an RGB image and returns an numpy array where
+    0 - Panel
+    1 - Background
+    2 - Dust
+    """
     patch_size = 256
-    image_size = (1792, 1024)
-    scale = 5
-    new_size = (image_size[0]//scale, image_size[1]//scale)
-    if ip_address:
-        cap = cv2.VideoCapture(ip_address)
-        ret, img = cap.read()
-        if not ret:
-            cprint("Failed to capture frame from stream", MC)
-            return # break if no next frame
-        cv2.waitKey(1)
-        img = cv2.resize(img, image_size)
-    elif image:
-        img = load_image(path_image, import_type='rgb_img') #load the image
-    else:
-        cprint("You must specify either ip or path to image", MC)
-        return
-    
     unet_model = model
 
     #Patches preprocessing
     cprint("Creating patches...", MC)
     small_patches = []
-    patches_img = patchify(img, (patch_size, patch_size, 3), step=patch_size) ##perform patchify
+    patches_img = patchify(image, (patch_size, patch_size, 3), step=patch_size) ##perform patchify
     for i in range(patches_img.shape[0]): 
         for j in range(patches_img.shape[1]): 
             single_patch_img = patches_img[i,j,:,:]
@@ -98,29 +88,33 @@ def unet_prediction(ip_address=None, image=None, model=None):
     cprint("Reconstructing image...", MC)
     patched_prediction = np.reshape(prediction_unet, [patches_img.shape[0], patches_img.shape[1], 
                                                       patches_img.shape[3], patches_img.shape[4]])
-    reconstructed_image = unpatchify(patched_prediction, (img.shape[0], img.shape[1]))
+    reconstructed_image = unpatchify(patched_prediction, (image.shape[0], image.shape[1]))
     
-    #OUTPUT: Plotting original and GT and prediction
+    return reconstructed_image
+    
+#OUTPUT: Plotting original and GT and prediction
+def show_images(original_image, dust_image):
     cprint("Plotting image...", MC)
-    cprint(type(reconstructed_image), MC)
-    original = cv2.resize(img.astype(np.uint8), new_size)
-    reconstructed = cv2.resize(reconstructed_image.astype(np.uint8), new_size) * 100
+    scale = 5
+    new_size = (image_size[0]//scale, image_size[1]//scale)
+    original = cv2.resize(original_image.astype(np.uint8), new_size)
+    original = cv2.cvtColor(original, cv2.COLOR_RGB2BGR)
+    
+    reconstructed = cv2.resize(dust_image.astype(np.uint8), new_size) * 100
     reconstructed = cv2.merge((reconstructed,reconstructed,reconstructed))
     twins = np.concatenate((original, reconstructed), axis=1)
     
-    cprint('Max value: ' + str(np.max(reconstructed_image)), MC)
+    cprint('Max value: ' + str(np.max(dust_image)), MC)
     cprint("Press any key on the image to exit", MC)
     cv2.namedWindow("Image vs Unet prediction", cv2.WINDOW_GUI_NORMAL | cv2.WINDOW_AUTOSIZE)
     cv2.imshow('Image vs Unet prediction', twins)
     cv2.waitKey(0)
-    
-    cap.release()
     cv2.destroyAllWindows()
     
     #plt.figure(figsize=(10, 6))
     #plt.subplot(231)
     #plt.title('Image')
-    #plt.imshow(img, cmap='gray')
+    #plt.imshow(image, cmap='gray')
     #plt.subplot(232)
     #plt.title('Unet prediction')
     #plt.imshow(reconstructed_image, cmap='gray')
@@ -129,16 +123,40 @@ def unet_prediction(ip_address=None, image=None, model=None):
 
 
 if __name__ == '__main__':
+    ##### EXCECUTION MODE
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-ip", help="ip of the images stream")
+    args = parser.parse_args()
+    
     ##### PATHS
     path_models="sm_unet4_03.hdf5" #path for hdf5 file with trained weights
-    path_image = "IMG_20221012_132227_DRO.png" #path for image
-    ip_address = "http://192.168.16.107:8000/stream.mjpg"
+    if args.ip:
+        cprint("Reading image from stream...", MC)
+        ip_address = f"http://{args.ip}:8000/stream.mjpg"
+        cap = cv2.VideoCapture(ip_address)
+        ret, img = cap.read()
+        if not ret:
+            cprint("Failed to capture frame from stream", MC)
+            sys.exit(1) # break if no next frame
+        cv2.waitKey(1)
+        img = cv2.resize(img, image_size)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        cap.release()
+    else:
+        cprint("Reading image from file...", MC)
+        path_image = "IMG_20221012_132227_DRO.png" #path for image
+        img = load_image(path_image, import_type='rgb_img') #load the image
 
+    #### LOAD KERAS
+    cprint("Loading keras...", MC)
+    import segmentation_models as sm
+    from keras.models import load_model
+    
     ##### LOAD THE MODEL (just run once because it takes too long due to load the pre-trained weights)
     cprint("Loading model...", MC)
     unet_model = load_model(path_models, compile=False)
 
-    ##### UNET_PREDICTION (run as needed, edit the path_image to predict in different images)
-    #unet_prediction(image=path_image, model=unet_model)
-    unet_prediction(ip_address=ip_address, model=unet_model)
-    
+    ##### UNET_PREDICTION (run as needed, edit the path_image to predict in different images)  
+    img_dust = unet_prediction(img, model=unet_model)
+    show_images(img, img_dust)
+
